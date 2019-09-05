216e7fcf45b2a8facc4b22b316e2ee18e81d7194
lisa grinberg <grinberg1165@gmail.com>
2019-09-05 22:48:34 +0000

    moveInches.py edited online with Bitbucket. Changes moveBlock object to turn function and move function to streamline the code. Turn function is incomplete, will finish later.

--- moveInches.py
+++ moveInches.py
@@ -2,7 +2,7 @@
 This will convert in/cm to rotations, be sure to use either metric or imperial for all params.
 TODO:
 add a default size once decided
-Please see the bottom of the code for a call example.
+add turning (see link in the turn function)
 """
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
@@ -19,24 +19,11 @@ Class moveBlock():
 		self.c = size        
 		self.distance = dist
 		self.speed = speed
-	def move(self):
+	def move(size, distance, speed = 75):
+		c = size        
+		distance = dist
+		speed = speed
 		r = d/c
 		tank_drive.on_for_rotations(SpeedPercent(speed), SpeedPercent(speed), r)
-	def turn(self):
+	def turn():
 	 #https://va-dcfll.org/distance-to-rotation-formula/ for formula for turning.
-	 
-"""
-How to call this object:
-1) initalize from this code:
-RIPGerhard = moveBlock(size of wheel, distance, desired speed (automatically at 75)
-RIPGerard.move()
-2) initialize from another code (change user/root to your home directory):
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
-import griffy-dev2/moveInches.py as program
-from program import moveBlock
-ParallelLinesHaveSoMuchInCommonItsAShameTheyNeverMeet = moveBlock(size of wheel, distance, desired speed (automatically at 75)
-ParallelLinesHaveSoMuchInCommonItsAShameTheyNeverMeet.move()
-"""
