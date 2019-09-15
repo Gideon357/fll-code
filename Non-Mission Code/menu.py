c4387cef6713982925cbc6ed813f7b5973cc8c47
Gideon Grinberg <gideongrinberg@lillianas-MacBook-Pro.local>
2019-09-15 15:24:23 -0400

    Made menu and menu object nested classes of loader and added menuItems to "loader" to allow for multiple menus. This will allow for a menu that chooses between file browser and motor control.

--- Non-Mission Code/menu.py
+++ Non-Mission Code/menu.py
@@ -10,55 +10,46 @@ from ev3dev2.led import Leds
 from ev3dev2.button import Button
 from ev3dev2.console import Console
 
-button = Button()
-menuItems = []
-displayNum = 0
-
-while True:
-    for i in range(len(menuItems)):
-        menuItems[i] = "nil"
-
-class menuObject:
-
-    def __init__(self, name, functionName, programNumber, programName):
-        self.functionName = functionName
-        self.programName = programName
-        self.programNumber = programNumber
-        self.name = name
-        menuItems[programNumber] = {"objectName": name, "programName":programName, "functionName":functionName}
-
-    def run(self):
-        functionName
-        currentProgram = currentProgram + 1 
-
-class menu:
+class loader:
     def __init__(self):
-        pass
-    def __del__(self):
-        pass
-    def on_left(self,state)
+        button = Button()
+        menuItems = []
+        displayNum = 0
         while True:
-            if state:
-                currentProgram = currentProgram - 1
-    def on_right(self, state)
-        while True:
-            if state:
-                currentProgram = currentProgram + 1
-    def on_enter(self)
-        while True:
-            objName = menuItems[currentProgram][objectName]
-            objName.run()
-    def display(self):
-        while True:
-            console.text_at(menuItems[currentProgram][programName], True)
+            for i in range(len(menuItems)):
+                menuItems[i] = "nil"
 
+    class menuObject:
 
+        def __init__(self, name, functionName, programNumber, programName):
+            self.functionName = functionName
+            self.programName = programName
+            self.programNumber = programNumber
+            self.name = name
+            menuItems[programNumber] = {"objectName": name, "programName":programName, "functionName":functionName}
 
+        def run(self):
+            functionName
+            currentProgram = currentProgram + 1 
 
-
-
-
-
-
-
-
+    class menu:
+        def __init__(self):
+            pass
+        def __del__(self):
+            pass
+        def on_left(self,state)
+            while True:
+                if state:
+                    currentProgram = currentProgram - 1
+        def on_right(self, state)
+            while True:
+                if state:
+                    currentProgram = currentProgram + 1
+        def on_enter(self)
+            while True:
+                objName = menuItems[currentProgram][objectName]
+                objName.run()
+        def display(self):
+            while True:
+                console.text_at(menuItems[currentProgram][programName], True)
+        
