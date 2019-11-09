1f90a0e3f5949f4eaa6f9a0b3bcc611a33a114c7
Nate Schmelkin <nateschmelkin@Nates-MacBook-Pro.local>
2019-11-09 13:55:33 -0500

    Changed missions.

--- Griffy/missions.py
+++ Griffy/missions.py
@@ -54,21 +54,22 @@ class Missions(Griffy):
     def fifth_run(self):
         """Broken building elevator and swing: 7"""
         # 90 degrees is `self.in_to_mm(1.8), self.in_to_mm(3.5)`
+        self.left_medium_motor.on_for_rotations(-100, .1) # Turn attachment
+        self.on_for_distance(SpeedPercent(50), 38, use_gyro=False) # Go to the beige circle
         self.left_medium_motor.on_for_rotations(100, .1) # Turn attachment
-        self.on_for_distance(SpeedPercent(50), 36, use_gyro=False) # Go to the beige circle
         self.on_for_distance(SpeedPercent(-20), 5, use_gyro=False) # Backup from the houses we put
         self.move_tank.on_for_rotations(15, -15, .15) # Turn past the houses
-        self.left_medium_motor.on_for_rotations(100, .28) # Turn attachment
-        self.move_tank.on_for_rotations(15, -15, .02) # Turn to the right
-        self.on_for_distance(SpeedPercent(30), 8.5, use_gyro=False) # Go forward 8.5 inches
+        self.left_medium_motor.on_for_rotations(-100, .45) # Turn attachment
+        self.move_tank.on_for_rotations(15, -15, .03) # Turn to the right
+        self.on_for_distance(SpeedPercent(40), 11, use_gyro=False) # Go forward 8.5 inches
         self.left_medium_motor.on_for_rotations(75, .125) # Turn attachment
-        self.move_tank.on_for_rotations(-15, 15, .085) # Tiny turn to adjust
         self.on_for_distance(SpeedPercent(30), 7, use_gyro=False) # Go forward 7 inches
-        # self.on_arc_right(SpeedPercent(50), self.in_to_mm(1.8), self.in_to_mm(1.2))
-        # self.on_arc_left(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(.5))
-        # self.on_for_distance(SpeedPercent(-20), 5, use_gyro=False)
-        # self.on_arc_right(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(2.65))
-        # self.on_for_distance(SpeedPercent(30), 7, use_gyro=False)
-        # self.on_for_distance(SpeedPercent(-30), 5, use_gyro=False)
-        # self.on_arc_right(SpeedPercent(-30), self.in_to_mm(1.8), self.in_to_mm(2.8))
-        # self.on_for_distance(-80, 55, use_gyro=False)
+        self.move_tank.on_for_rotations(15, -15, .1) # Turn to the right
+        self.move_tank.on_for_rotations(-15, 15, .1) # Turn to the left
+        self.on_for_distance(SpeedPercent(-20), 4, use_gyro=False)
+        self.on_arc_right(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(2.45))
+        self.on_for_distance(SpeedPercent(30), 7, use_gyro=False)
+        self.on_arc_left(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(1))
+        self.on_for_distance(SpeedPercent(-30), 6, use_gyro=False)
+        self.on_arc_right(SpeedPercent(-30), self.in_to_mm(1.8), self.in_to_mm(2.4))
+        self.on_for_distance(-80, 60, use_gyro=False)
