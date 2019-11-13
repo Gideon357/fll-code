0da0aee6589f1ac720d08ed284f52619d274f79b
Gideon Grinberg <gearheadgriffins@gmail.com>
2019-11-12 20:24:47 -0500

    Adds ramp

--- Griffy/missions.py
+++ Griffy/missions.py
@@ -59,25 +59,31 @@ class Missions(Griffy):
     def fifth_run(self):
         """Broken building elevator and swing: 7"""
         # 90 degrees is `self.in_to_mm(1.8), self.in_to_mm(3.5)`
-        #self.left_medium_motor.on_for_rotations(-100, .1) # Turn attachment
-        #self.on_for_distance(SpeedPercent(50), 38, use_gyro=False) # Go to the beige circle
-        #self.left_medium_motor.on_for_rotations(100, .1) # Turn attachment
-        #self.on_for_distance(SpeedPercent(-20), 5, use_gyro=False) # Backup from the houses we put
-        #self.move_tank.on_for_rotations(15, -15, .15) # Turn past the houses
-        #self.left_medium_motor.on_for_rotations(-100, .45) # Turn attachment
-        #self.move_tank.on_for_rotations(15, -15, .03) # Turn to the right
-        #self.on_for_distance(SpeedPercent(40), 11, use_gyro=False) # Go forward 8.5 inches
-        #self.left_medium_motor.on_for_rotations(75, .125) # Turn attachment
-        #self.on_for_distance(SpeedPercent(30), 7, use_gyro=False) # Go forward 7 inches
-        # self.move_tank.on_for_rotations(15, -15, .1) # Turn to the right
-        # self.move_tank.on_for_rotations(-15, 15, .1) # Turn to the left
-        # self.on_for_distance(SpeedPercent(-20), 4, use_gyro=False)
+        self.left_medium_motor.on_for_rotations(-100, .1) # Turn attachment
+        self.on_for_distance(SpeedPercent(50), 38, use_gyro=False) # Go to the beige circle
+        self.left_medium_motor.on_for_rotations(100, .1) # Turn attachment
+        self.on_for_distance(SpeedPercent(-20), 5, use_gyro=False) # Backup from the houses we put
+        self.move_tank.on_for_rotations(15, -15, .15) # Turn past the houses
+        self.left_medium_motor.on_for_rotations(-100, .45) # Turn attachment
+        self.move_tank.on_for_rotations(15, -15, .03) # Turn to the right
+        self.on_for_distance(SpeedPercent(40), 11, use_gyro=False) # Go forward 8.5 inches
+        self.left_medium_motor.on_for_rotations(75, .125) # Turn attachment
+        self.on_for_distance(SpeedPercent(30), 7, use_gyro=False) # Go forward 7 inches
+        self.move_tank.on_for_rotations(15, -15, .1) # Turn to the right
+        self.move_tank.on_for_rotations(-15, 15, .1) # Turn to the left
+        self.on_for_distance(SpeedPercent(-20), 4, use_gyro=False)
         self.on_arc_right(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(2.45))
         self.on_for_distance(SpeedPercent(30), 7, use_gyro=False)
         self.on_arc_left(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(1))
         self.on_for_distance(SpeedPercent(-30), 6, use_gyro=False)
         self.on_arc_right(SpeedPercent(-30), self.in_to_mm(1.8), self.in_to_mm(2.4))
         self.on_for_distance(-80, 60, use_gyro=False)
+    def ramp(self):
+        self.drive_until_white_black(100)
+        self.turn_left(100,90)
+        self.on_for_distance(100,self.in_to_mm(34),)
+
+
 
     def resetGyro(self, port, timeout=-1):
         endTime = time.time() + timeout
@@ -99,3 +105,5 @@ class Missions(Griffy):
             print('Reset failed. Resetting again.')
 
     resetGyro('in1')
+
+
