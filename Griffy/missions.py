eb6dcedfa66f49dbb2b7809b276be9130afa8a93
Gideon Grinberg <gearheadgriffins@gmail.com>
2019-11-12 19:21:21 -0500

    Adds a gyro calibrate script to menu.

--- Griffy/missions.py
+++ Griffy/missions.py
@@ -3,13 +3,15 @@
 from .griffy import Griffy
 from .button import Button
 from ev3dev2.motor import SpeedPercent
-from time import sleep
 from sys import stderr
 from ev3dev2.button import Button
 from ev3dev2.console import Console
 from ev3dev2.led import Leds
 from ev3dev2.sensor import list_sensors, INPUT_1, INPUT_2, INPUT_3, INPUT_4
-
+from ev3dev2.port import *
+from ev3dev2.sensor.lego import *
+from time import sleep
+import time
 
 class Missions(Griffy):
     """
@@ -57,22 +59,43 @@ class Missions(Griffy):
     def fifth_run(self):
         """Broken building elevator and swing: 7"""
         # 90 degrees is `self.in_to_mm(1.8), self.in_to_mm(3.5)`
-        self.left_medium_motor.on_for_rotations(-100, .1) # Turn attachment
-        self.on_for_distance(SpeedPercent(50), 38, use_gyro=False) # Go to the beige circle
-        self.left_medium_motor.on_for_rotations(100, .1) # Turn attachment
-        self.on_for_distance(SpeedPercent(-20), 5, use_gyro=False) # Backup from the houses we put
-        self.move_tank.on_for_rotations(15, -15, .15) # Turn past the houses
-        self.left_medium_motor.on_for_rotations(-100, .45) # Turn attachment
-        self.move_tank.on_for_rotations(15, -15, .03) # Turn to the right
-        self.on_for_distance(SpeedPercent(40), 11, use_gyro=False) # Go forward 8.5 inches
-        self.left_medium_motor.on_for_rotations(75, .125) # Turn attachment
-        self.on_for_distance(SpeedPercent(30), 7, use_gyro=False) # Go forward 7 inches
-        self.move_tank.on_for_rotations(15, -15, .1) # Turn to the right
-        self.move_tank.on_for_rotations(-15, 15, .1) # Turn to the left
-        self.on_for_distance(SpeedPercent(-20), 4, use_gyro=False)
+        #self.left_medium_motor.on_for_rotations(-100, .1) # Turn attachment
+        #self.on_for_distance(SpeedPercent(50), 38, use_gyro=False) # Go to the beige circle
+        #self.left_medium_motor.on_for_rotations(100, .1) # Turn attachment
+        #self.on_for_distance(SpeedPercent(-20), 5, use_gyro=False) # Backup from the houses we put
+        #self.move_tank.on_for_rotations(15, -15, .15) # Turn past the houses
+        #self.left_medium_motor.on_for_rotations(-100, .45) # Turn attachment
+        #self.move_tank.on_for_rotations(15, -15, .03) # Turn to the right
+        #self.on_for_distance(SpeedPercent(40), 11, use_gyro=False) # Go forward 8.5 inches
+        #self.left_medium_motor.on_for_rotations(75, .125) # Turn attachment
+        #self.on_for_distance(SpeedPercent(30), 7, use_gyro=False) # Go forward 7 inches
+        # self.move_tank.on_for_rotations(15, -15, .1) # Turn to the right
+        # self.move_tank.on_for_rotations(-15, 15, .1) # Turn to the left
+        # self.on_for_distance(SpeedPercent(-20), 4, use_gyro=False)
         self.on_arc_right(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(2.45))
         self.on_for_distance(SpeedPercent(30), 7, use_gyro=False)
         self.on_arc_left(SpeedPercent(30), self.in_to_mm(1.8), self.in_to_mm(1))
         self.on_for_distance(SpeedPercent(-30), 6, use_gyro=False)
         self.on_arc_right(SpeedPercent(-30), self.in_to_mm(1.8), self.in_to_mm(2.4))
         self.on_for_distance(-80, 60, use_gyro=False)
+
+    def resetGyro(self, port, timeout=-1):
+        endTime = time.time() + timeout
+        while True:
+            print('Resetting port')
+            legoPort = LegoPort(port)
+            legoPort.mode = 'auto'
+            for _ in range(10):
+                if time.time() > endTime and timeout > 0:
+                    print('Reset failed. Timeout reached')
+                    return
+                time.sleep(1)
+                try:
+                    GyroSensor(port)
+                    print('Gyro detected')
+                    return
+                except:
+                    print('Gyro not detected. Trying again.')
+            print('Reset failed. Resetting again.')
+
+    resetGyro('in1')
